834a07e57b5df0da4188e57da5afd8bd7b2692bc
Jared <razrsword@gmail.com>
2019-03-23 13:17:25 -0400

    More cleanup

--- src/main/java/com/jared/lineserver/LineServerApplication.java
+++ src/main/java/com/jared/lineserver/LineServerApplication.java
@@ -4,11 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableCaching
 class LineServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LineServerApplication.class, args);
 	}
-
 }
